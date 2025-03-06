a479b5dc276394fbe266b3ef0e5a62b2aa5ad6e2
Connor Phelps <phelpsconnor05@gmail.com>
2025-03-05 17:24:00 -0700

    created service folder and added authentication endpoints

---
+++ service/index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const app = express();
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+let users = {};
+
+app.get('*', (_req, res) => {
+  res.send({ msg: 'Simon service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+app.use(express.json());
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await createUser(req.body.email, req.body.password);
+  
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+    }
+});
+  
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+  
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+  
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      next();
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    users.push(user);
+  
+    return user;
+}
+  
+async function findUser(field, value) {
+    if (!value) return null;
+  
+    return users.find((u) => u[field] === value);
+}
+  
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+}
+
